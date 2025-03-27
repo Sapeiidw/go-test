dfadd373d9e1afead9d02d24dd561b879d09cc09
Sapeiidw <sapeiidw@gmail.com>
2025-03-27 23:13:26 +0800

    add : recover middleware

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"go-test/config"
 	"go-test/controllers"
+	"go-test/middleware"
 	"go-test/repository"
 	"go-test/routes"
 	"go-test/services"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
@@ -15,11 +17,21 @@ func main() {
 
 	app := fiber.New()
 
+	// Apply logging middleware
+	app.Use(recover.New(recover.Config{
+		EnableStackTrace: true, // Show stack trace in logs (useful for debugging)
+	})) // ✅ This will catch any panics and prevent crashes
+	app.Use(middleware.LoggerMiddleware())
+
+
+	// Setup Dependencies
 	userRepo := repository.NewUserRepository(config.DB)
 	userService := services.NewUserService(userRepo)
 	userController := controllers.NewUserController(userService)
 
 	routes.SetupUserRoutes(app, userController)
+
+
 
 	app.Listen(":3000")
 }
